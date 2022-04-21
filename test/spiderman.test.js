f7acd3d078186b5e943b676ef16e9d1007e9e75a
JaviMx <javimxmine@hotmail.com>
2022-04-21 17:33:48 -0500

    Prueba Mi turno hecha

--- test/spiderman.test.js
+++ test/spiderman.test.js
@@ -14,4 +14,9 @@ describe("Test Suite Dummy Description", () => {
         expect(andrewGarfield.studio).toBe("Sony")
 
     });
+
+    test("2. Use the method getInfo()", () =>{
+        const tomHolland = new Spiderman("Spiderman Marvel", 25, "Tom Holland", 5, "Marvel")
+        expect(tomHolland.getInfo()).toBe("Hey, I'm Tom Holland from Marvel studio")
+    });
   })
